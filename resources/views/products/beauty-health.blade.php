8613d46775e0b8c42b0dc9de734359fd0255b134
ajhmdni <ajhmdni02@gmail.com>
2024-02-25 20:55:27 +0700

    (update): add views

---
+++ resources/views/products/beauty-health.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>POS | Beauty Health</title>
+</head>
+<body>
+  <h1>Beauty Health</h1>
+  <p>Health will lead to beauty</p>
+</body>
+</html>
---
+++ resources/views/products/home-care.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>POS | Home Care</title>
+</head>
+<body>
+  <h1>Home Care</h1>
+  <p>Care about the area around you</p>
+</body>
+</html>
